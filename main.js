b79a5730e67eb586b36fd72a73e9b29da2217865
Samandar <zafarovsamandar5444@gmail.com>
2024-10-14 14:24:14 +0500

    12345678

---
+++ main.js
@@ -0,0 +1,36 @@
+let ism = prompt("Ismingizni kiriting!");
+let familya = prompt("Familyangizni kiriting!");
+let yoshi = +prompt("Yoshingizni kiriting!");
+let yashashJoyi = prompt("Manzilingizni kiriting!");
+let telefonNomer = +prompt("Telefon nomerni kiriting!");
+let oqidiganJoyi = prompt("oqiydigan joyingiz yoki ish joyingizni kiriting!");
+let tugilganOY = prompt("Tugilgan oyingizni  kiriting!");
+let tugilganYil = +prompt("Tugilgan yilingizni kiriting!");
+let kampyuterNomi = prompt("kompyuteringizni nomini kiriting!");
+let turmushQurganmisiz = prompt("Turmush qurganmisz!");
+
+console.log(`
+     Sizning ismigiz: ${ism},
+     Sizning familyangiz: ${familya},
+     Sizning yoshingiz: ${yoshi},
+     Sizning yashash joyingiz :${yashashJoyi},
+     Sizning telefon raqamingiz: ${telefonNomer},
+     Sizning oqiydigan joyingiz yoki ishlidigan: ${oqidiganJoyi},
+     Sizning tugilgan oyingiz: ${tugilganOY},
+     Sizning tugilgan yilingiz: ${tugilganYil},
+     Sizning kampyuterimgizni nomi: ${kampyuterNomi},
+     Turmush qurish : ${turmushQurganmisiz},
+    `);
+
+alert(`
+   Sizning ismigiz: ${ism},
+     Sizning familyangiz: ${familya},
+     Sizning yoshingiz: ${yoshi},
+     Sizning yashash joyingiz :${yashashJoyi},
+     Sizning telefon raqamingiz: ${telefonNomer},
+     Sizning oqiydigan joyingiz yoki ishlidigan: ${oqidiganJoyi},
+     Sizning tugilgan oyingiz: ${tugilganOY},
+     Sizning tugilgan yilingiz: ${tugilganYil},
+     Sizning kampyuterimgizni nomi: ${kampyuterNomi},
+     Turmush qurish : ${turmushQurganmisiz},
+   `);
